6b313bc7174b6c10be99bc7ce715875b519148c3
Manu Herrera <manuherrera8@gmail.com>
2024-03-19 11:40:39 -0300

    Add support for OSLog (#9)
    * Add support for OSLog

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CoreNetworking",
     platforms: [
-        .iOS(.v15),
+        .iOS(.v16),
         .macOS(.v12)
     ],
     products: [
